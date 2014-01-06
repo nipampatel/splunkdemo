89564309bdd35fb3b6003d63c2a6ec417003bffe
Nipam Patel <nipam2000@gmail.com>
2014-01-06 16:16:24 -0500

    Added unique id

--- splunkdemo/Controllers/HomeController.cs
+++ splunkdemo/Controllers/HomeController.cs
@@ -11,32 +11,26 @@ namespace splunkdemo.Controllers
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-        public HomeController()
-        {            
-        }        
-
         public ActionResult Index()
-        {
-            _logger.Log(LogLevel.Warn, "Index page hit");
+        {            
             return View();
         }
 
         public ActionResult Login(string errorCode)
-        {
-            // errorCode refers to user name for this demo
-            _logger.Log(LogLevel.Info, string.Format("User={0}, IP=127.0.0.1", errorCode));
+        {   
+            _logger.Log(LogLevel.Info, string.Format("Id={0}, User={1}", HttpContext.Items["uniqueid"], errorCode));
             return new JsonResult() { Data = "Login Successful", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
         public ActionResult ThrowHttpException(string errorCode)
         {
-            _logger.LogException(LogLevel.Error, string.Format("StatusCode={0}", errorCode), new HttpException(Int32.Parse(errorCode), "Some sort of description for exception goes here"));
+            _logger.LogException(LogLevel.Error, string.Format("Id={0}, StatusCode={1}", HttpContext.Items["uniqueid"], errorCode), new HttpException(Int32.Parse(errorCode), "Some sort of description for exception goes here"));
             return View(Int32.Parse(errorCode));
         }
 
         public ActionResult LogKVP(string errorCode)
         {
-            _logger.Log(LogLevel.Info, string.Format("Name={0}, ReferrerUrl={1}", errorCode, System.Web.HttpContext.Current.Request.UrlReferrer));
+            _logger.Log(LogLevel.Info, string.Format("Id={0}, Name={1}", HttpContext.Items["uniqueid"], errorCode));
             return new JsonResult() { Data = "Kvp Logged to info level.", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
---
+++ splunkdemo/Modules/LoggerModule.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace splunkdemo.Modules
+{
+    public class LoggerModule : IHttpModule
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            
+            context.BeginRequest += OnStartRequest;
+            context.EndRequest += OnEndRequest;
+        }
+
+        public void OnStartRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Items["uniqueid"] = Guid.NewGuid().ToString("N");
+        }
+
+        public void OnEndRequest(object sender, EventArgs e)
+        {
+            HttpApplication context = (HttpApplication)sender;
+            RouteData routeData = context.Request.RequestContext.RouteData;
+
+            _logger.Log(LogLevel.Info, string.Format("Id={0},IpAddress={1},RequestUrl={2},Status={3},Referrer={4},BrowserType={5}", 
+                HttpContext.Current.Items["uniqueid"],
+                context.Request.ServerVariables["REMOTE_ADDR"],
+                context.Request.Url,
+                context.Response.Status,
+                (context.Request.UrlReferrer == null ? string.Empty : context.Request.UrlReferrer.ToString()),
+                context.Request.UserAgent));
+        }
+    }
+}
